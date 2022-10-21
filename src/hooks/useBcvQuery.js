98b683f2ef3e6b2d68a2080c5f0484fe4ed22a91
larsgson <4819617+larsgson@users.noreply.github.com>
2022-10-21 09:36:08 +0200

    added new hook file

---
+++ src/hooks/useBcvQuery.js
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import usePerf from "./usePerf";
+import usePkResourceCache from './usePkResourceCache';
+import { AppContext } from '@context/AppContext'
+
+export default function useBcvQuery({ 
+  bcvQuery
+}) {
+  const state = {
+  };
+
+  const actions = {
+  };
+
+  return { state, actions };
+
+}
