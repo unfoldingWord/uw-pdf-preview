4a63ebe6002bed827c7854847203f996b31bdc5b
larsgson <4819617+larsgson@users.noreply.github.com>
2022-11-08 17:00:23 +0100

    first test compile successful

--- src/components/PdfPreview.jsx
+++ src/components/PdfPreview.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types';
 // import { useDeepCompareCallback, useDeepCompareEffect } from "use-deep-compare";
 // import isEqual from 'lodash.isequal';
-import {
-  useCatalog, useRenderPreview,
-} from 'proskomma-react-hooks'
+// import {
+//   useCatalog, useRenderPreview,
+// } from 'proskomma-react-hooks'
 
 export default function PdfPreview( props) {
-  const { bookId, verbose } = props;
+  // const { bookId, verbose } = props;
 
   // const pdfPreviewProps = {
   //   // handlers,
