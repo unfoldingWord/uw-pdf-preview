a71c8bd7082f7c3e3713a2db5a1fbbfb1b454c34
larsgson <4819617+larsgson@users.noreply.github.com>
2022-11-11 17:41:18 +0100

    some draft preparations for later - (maybe to be used, or not..)

--- src/context/LocalPkCacheContext.js
+++ src/context/LocalPkCacheContext.js
@@ -1,6 +1,7 @@
 import React, {
   createContext,
   useState,
+  useEffect,
 } from 'react'
 import PropTypes from 'prop-types'
 
@@ -13,6 +14,50 @@ export default function PkCacheProvider({ children }) {
   const [epCache,setEpCache] = useState({})
 
   const pkHook = useProskomma({verbose: true})
+
+  const [printPreview, setPrintPreview] = useState(false)
+  const [html, setHtml] = useState(null);
+
+  useEffect(() => {
+    if ( html ) {
+      console.log("html data is available")
+      const newPage = window.open('','','_window');
+      newPage.document.head.innerHTML = "<title>PDF Preview</title>";
+      const script = newPage.document.createElement('script');
+      script.src = 'https://unpkg.com/pagedjs/dist/paged.polyfill.js';
+      newPage.document.head.appendChild(script);
+      const style = newPage.document.createElement('style');
+      const newStyles = `
+      body {
+        margin: 0;
+        background: grey;
+      }
+      .pagedjs_pages {
+      }
+      .pagedjs_page {
+        background: white;
+        margin: 1em;
+      }
+      .pagedjs_right_page {
+        float: right;
+      }
+      .pagedjs_left_page {
+        float: left;
+      }
+      div#page-2 {
+        clear: right;
+      }
+      `;
+      style.innerHTML = newStyles + html.replace(/^[\s\S]*<style>/, "").replace(/<\/style>[\s\S]*/, "");
+      newPage.document.head.appendChild(style);
+      newPage.document.body.innerHTML = html.replace(/^[\s\S]*<body>/, "").replace(/<\/body>[\s\S]*/, "");      
+      setHtml(null);
+    }
+  }, [html])
+
+
+
+
 
   // create the value for the context provider
   const context = {
